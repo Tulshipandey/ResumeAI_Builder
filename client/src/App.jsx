6246a4d4952d48f68085f1f609cfdcd8080f9e3b
Tulshipandey <tulshiprashadpandey892@gmail.com>
2025-10-28 01:46:11 +0530

    fix: Correct import case for Preview and Login components in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
-import Preview from "./pages/preview";
+import Preview from "./pages/Preview";
 import ResumeBuilder from "./pages/ResumeBuilder";
 import Dashboard from "./pages/Dashboard";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Layout from './pages/Layout';
 
 const App = () => {
